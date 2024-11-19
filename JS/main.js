5df73c299ba4f7d98a373ad63ca2e5a6595148b4
ilemrevollo <revolloilem@gmail.com>
2024-11-19 14:18:52 -0400

    Fer2007

---
+++ JS/main.js
@@ -0,0 +1,42 @@
+let mensaje="Picachu atack trueno";
+console.log (mensaje);
+let numerouno=2;
+let numerodos=4;
+let numerotres=10;
+let resultado= numerouno+numerodos+numerotres;
+console.log ("la suma de los numeros es" , resultado)
+let resultado2= numerouno-numerodos-numerotres;
+console.log ("la resta de los numeros es" , resultado2)
+let resultado3= numerouno*numerodos*numerotres;
+console.log ("la multiplicacion de los numeros es" , resultado3)
+let resultado4= numerouno/numerodos/numerotres;
+console.log ("la division de los numeros es" , resultado4)
+function sumar ()
+{
+let numerouno=2;
+let numerodos=4;
+let numerotres=10;
+let resultado= numerouno+numerodos+numerotres;
+alert ("la suma es"+ resultado)}
+function restar ()
+{
+let numerouno=2;
+let numerodos=4;
+let numerotres=10;
+let resultado2= numerouno-numerodos-numerotres;
+alert ("la resta es "+ resultado2)
+}
+function multiplicar ()
+{
+let numerouno=2;
+let numerodos=4;
+let numerotres=10;
+let resultado= numerouno*numerodos*numerotres;
+alert ("la multiplicacion es "+ resultado3)}
+function dividir ()
+{
+let numerouno=2;
+let numerodos=4;
+let numerotres=10;
+let resultado= numerouno/numerodos/numerotres;
+alert ("la division es "+ resultado4)}
